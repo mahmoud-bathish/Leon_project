8e29c99a80603d83bb131ba9d9b99bcd99ae06c9
Mahmoud Bathich <mbathich@assentify.com>
2023-06-03 19:01:24 +0300

    add-parallax-scrolling-effect

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 let icon = document.getElementById('icon');
-let links = document.querySelector('.links ul')
-console.log(links)
+let links = document.querySelector('.links ul');
+let dynamicLine = document.getElementById('dynamicLine');
 icon.addEventListener('click',()=>{
     links.classList.toggle('activee');
-})
+    dynamicLine.classList.toggle('increase-width');
+});
+
